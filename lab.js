b81b4bdb866cab08ed900d9b7e8529743d15e03d
anastasia15r <nastaplotnik98@gmail.com>
2025-02-26 22:00:16 +0200

    Add files via upload

---
+++ lab.js
@@ -0,0 +1,34 @@
+// Выводим сообщение в консоль
+console.log("Привет, мир!");
+
+// Операция сложения
+console.log(2 + 3);
+
+// Всплывающее окно с приветствием
+alert("Привет, мир!");
+
+// Переменные
+let name = "anastasia";  // строка
+let year = 2005;    // год рождения
+let isStudent = true; // Логическое значение (True или False)
+
+// Вывод переменных в консоль
+console.log("Имя: " + name);
+console.log("Год рождения: " + year);
+console.log("Студент? " + isStudent);
+
+// Простой пример условий
+let score = 85; // Балл
+
+if (score >= 90) {
+  console.log("Отлично!");
+} else if (score >= 70) {
+  console.log("Хорошо!");
+} else {
+  console.log("Нужно улучшить!");
+}
+
+// Простой цикл
+for (let i = 1; i <= 5; i++) {
+  console.log("Итерация: " + i);
+}
